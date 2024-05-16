2db5c6ddf23e6c2bd6dcf503ea00c3d9cf259c48
amzpapag <73858359+amzpapag@users.noreply.github.com>
2024-05-16 11:25:30 +0100

    Add files via upload

---
+++ js/chall4.js
@@ -0,0 +1,32 @@
+function challenge4(){
+    console.log("Made it");
+    e.preventDefault(); 
+}
+//     var nameField = document.querySelector("#name").value;
+//     var addrField = document.querySelector("#addr").value;
+
+// console.log("Name is" + nameIn);
+// console.log("address is" + addressIn);
+
+//     nameError = document.querySelector("#nameError")
+
+//     addrError = document.querySelector("#addrError")
+   
+   
+//     if (nameField.length ===0 || addrField.length ===0){
+   
+        
+
+//         if (nameField.length === 0) {
+//             nameError.style.display = "block"; // Show for name field if it's empty
+//         } else {
+//             nameError.style.display = "none"; // Hide the error message for name field if it's not empty
+//         }
+
+//         if (addrField.length === 0) {
+//             addrError.style.display = "block"; // Show the error message for address field if it's empty
+//         } else {
+//             addrError.style.display = "none"; // Hide the error message for address field if it's not empty
+//         }
+//     }
+// }
